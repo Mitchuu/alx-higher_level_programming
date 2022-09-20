4fe42575600ea67b617cf494e5d35085b6cad30b
Mitchuu <anyangomitchelle7@yahoo.com>
2022-09-20 15:47:48 -0700

    Task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 
-
 /**
  * insert_node - Inserts a number into a sorted singly linked list
  * @head: Pointer the list
@@ -10,64 +9,64 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-listint_t *insert;
-listint_t *node;
+	listint_t *insert;
+	listint_t *node;
 
-insert = *head;
+	insert = *head;
 
-node = malloc(sizeof(listint_t));
-if (!node)
-return (NULL);
+	node = malloc(sizeof(listint_t));
+	if (!node)
+		return (NULL);
 
-node->n = number;
-node->next = NULL;
+	node->n = number;
+	node->next = NULL;
 
-if (*head == NULL)
-{
-*head = node;
-return (node);
-}
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
 
-if (insert->next == NULL)
-{
-if (insert->n < number)
-{
-insert->next = node;
-return (node);
-}
-else
-{
-node->next = insert;
-*head = node;
-return (node);
-}
-}
+	if (insert->next == NULL)
+	{
+		if (insert->n < number)
+		{
+			insert->next = node;
+			return (node);
+		}
+		else
+		{
+			node->next = insert;
+			*head = node;
+			return (node);
+		}
+	}
 
-while (insert)
-{
-if (insert->next)
-{
-if (insert->n > number)
-{
-node->next = insert;
-*head = node;
-break;
-}
-else if (insert->next->n > number)
-{
-node->next = insert->next;
-insert->next = node;
-break;
-}
-else
-insert = insert->next;
-}
-else
-{
-node->next = NULL;
-insert->next = node;
-break;
-}
-}
-return (node);
+	while (insert)
+	{
+		if (insert->next)
+		{
+			if (insert->n > number)
+			{
+				node->next = insert;
+				*head = node;
+				break;
+			}
+		    else if (insert->next->n > number)
+			{
+				node->next = insert->next;
+				insert->next = node;
+				break;
+		    }
+			else
+				insert = insert->next;
+		}
+		else
+		{
+			node->next = NULL;
+			insert->next = node;
+			break;
+		}
+	}
+	return (node);
 }
